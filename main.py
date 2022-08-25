55962072ad2debf6bbf6a4d0fed264d595c14296
webnait <111684818+webnait@users.noreply.github.com>
2022-08-25 17:31:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def get_weather2():
   url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
   res = requests.get(url).json()
   weather = res['data']['list'][0]
-  return math.floor(weather['low']),math.floor(weather['high']),weather['date']
+  return math.floor(weather['low']),math.floor(weather['high']),str(weather['date'])
 
 def get_count():
   delta = today - datetime.strptime(start_date, "%Y-%m-%d")
@@ -55,7 +55,7 @@ client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
 wea, temperature= get_weather()
-low,high,date= get_weather2()
-data = {"open_word":{"value":o_w,"color":get_random_color()},"today":{"value":date,"color":get_random_color()},"city":{"value":city,"color":get_random_color()},"weather":{"value":wea},"temperature":{"value":temperature},"low":{"value":low,"color":get_random_color()},"high":{"value":high,"color":get_random_color()},"love_days":{"value":get_count(),"color":get_random_color()},"birthday_left":{"value":get_birthday(),"color":get_random_color()},"words":{"value":get_words(), "color":get_random_color()},"chengzi":{"value":cz,"color":get_random_color()}}
+low,high,date_t= get_weather2()
+data = {"open_word":{"value":o_w,"color":get_random_color()},"today":{"value":date_t,"color":get_random_color()},"city":{"value":city,"color":get_random_color()},"weather":{"value":wea},"temperature":{"value":temperature},"low":{"value":low,"color":get_random_color()},"high":{"value":high,"color":get_random_color()},"love_days":{"value":get_count(),"color":get_random_color()},"birthday_left":{"value":get_birthday(),"color":get_random_color()},"words":{"value":get_words(), "color":get_random_color()},"chengzi":{"value":cz,"color":get_random_color()}}
 res = wm.send_template(user_id, template_id, data)
 print(res)
